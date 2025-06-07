3942a19688a22b01b1697d456a094c4f4ad7f1ec
Iraklis Chatzinikolaou <iraklis.chatz@gmail.com>
2025-06-07 13:59:13 +0300

    change conftest

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,13 +1,12 @@
-import sys
 import os
+import sys
+
 import pytest
 
 # Add /app to sys.path so we can import app.py
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app import app as flask_app  # ✅ this should now work
-
-
 
 
 @pytest.fixture

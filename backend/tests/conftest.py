696a14429cee0984ca619e87b7131187cc1e09b7
Iraklis Chatzinikolaou <iraklis.chatz@gmail.com>
2025-06-08 20:01:34 +0300

    yes it works

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,13 +1,17 @@
 import os
 import sys
 
+print("Current working directory:", os.getcwd())
+print("Directory listing of /app:", os.listdir("/app"))
+print("Directory listing of /app/tests:", os.listdir(os.path.dirname(__file__)))
 import pytest
+from main import app as flask_app  # noqa: E402
 
 # Add /app to sys.path so we can import app.py
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from app import app as flask_app  # noqa: E402
-# print("flask_app type:", type(flask_app))
+print("flask_app type:", type(flask_app))
+print("flask_app repr:", repr(flask_app))
 
 
 @pytest.fixture
